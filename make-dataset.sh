11e5a9e7345edf4c5f7843e507a1f297ddd20cbd
Alexander Hallard <zanda.hallard@outlook.com>
2022-10-18 22:13:58 +0100

    UNTESTED Added make-dataset.sh to move files to yolo directory structure

---
+++ make-dataset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -p "Enter dataset path" fsocoPATH
+read -p "Enter new dataset path" newPATH
+
+cd $fsocoPATH
+
+mkdir -p $newPATH{images,images_val,labels}
+
+cp $fsocoPATH/*/img/* $newPATH/images
+
+cp $fsocoPATH/*/ann/* $newPATH/labels
+
+cd $newPATH
+
+ls images | shuf -n 1152 > randomset
+
+for f in $(cat randomset); do
+    mv images/$f images_val;
+done
+
+sed -i -e 's/jpg/txt/g' randomset
+
+while read f; do mv labels/$f images_val/; done < randomset
